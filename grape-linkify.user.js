6103ba2ce37b8e508c4b96b2b84a23405b4c0896
Florian Preinstorfer <site-github@nblock.org>
2020-09-25 10:53:32 +0200

    Add grape-linkify script

---
+++ grape-linkify.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name        Grape: Linkify
+// @description Add backlinks to service integrations such as from Grape to Jira
+// @namespace   https://nblock.org
+// @match       https://uebergrape.staging.chatgrape.com/chat/channel/*
+// @grant       none
+// @run-at      document-idle
+// @downloadURL https://raw.githubusercontent.com/nblock/userscripts/master/grape-linkify.user.js
+// @updateURL   https://raw.githubusercontent.com/nblock/userscripts/master/grape-linkify.user.js
+// @author      nblock <site-github@nblock.org>
+// @version     0.1
+// ==/UserScript==
+
+var jira_regex = /(^[A-Z]+-[0-9]+) .*/;
+var jira_url = "https://jira.ubergrape.com/browse/";
+
+linkify_jira = function(mutation) {
+  var title = document.querySelector('main > div:first-child h1');
+  var mo = jira_regex.exec(title.innerHTML);
+  if (Array.isArray(mo) && mo.length > 1) {
+    title.innerHTML = '<a href="' + jira_url + mo[1] + '">' + mo[0] + '</a>';
+  }
+}
+
+var observer = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+    linkify_jira(mutation);
+  });
+});
+observer.observe(document.querySelector('head > title'), {childList: true});
