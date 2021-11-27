5125e588e42b06739b74c753e814f0c242030617
Florian Preinstorfer <site-github@nblock.org>
2021-11-27 14:45:17 +0100

    Access form directly

--- zabbix-permalink.user.js
+++ zabbix-permalink.user.js
@@ -5,23 +5,22 @@
 // @match       https://zabbix.example.com/*
 // @grant       none
 // @author      nblock <site-github@nblock.org>
-// @version     0.1
+// @version     0.2
 // ==/UserScript==
 
 // NOTE: Add your Zabbix URLs above with one or more @match blocks.
 
-document.querySelectorAll('form').forEach((form) => {
-  if (form.name === 'zbx_filter') {
-    const url = new URL(location.pathname, location.origin);
-    url.search = new URLSearchParams(new FormData(form)).toString();
+var form = document.forms.zbx_filter;
+if (form != null) {
+  const url = new URL(location.pathname, location.origin);
+  url.search = new URLSearchParams(new FormData(form)).toString();
 
-    var a = document.createElement('a');
-    a.appendChild(document.createTextNode('Permalink'));
-    a.title = 'Permalink with currently applied filters';
-    a.href = url.toString();
+  var a = document.createElement('a');
+  a.appendChild(document.createTextNode('Permalink'));
+  a.title = 'Permalink with currently applied filters';
+  a.href = url.toString();
 
-    var getFooter = document.getElementsByTagName('footer')[0];
-    getFooter.insertAdjacentHTML('beforeend', ', ');
-    getFooter.insertAdjacentHTML('beforeend', a.outerHTML);
-  }
-});
+  var getFooter = document.getElementsByTagName('footer')[0];
+  getFooter.insertAdjacentHTML('beforeend', ', ');
+  getFooter.insertAdjacentHTML('beforeend', a.outerHTML);
+}
